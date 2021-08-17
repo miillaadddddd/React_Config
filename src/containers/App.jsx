9d656e2c2f3ac39060adbec05d6c2eab1b3c3953
miillaadddddd <milad7212@gmail.com>
2021-08-17 13:00:29 +0430

    Config React

---
+++ src/containers/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App = () => {
+    return (<div className="rtl">
+    
+    <h1>این فایل کانفیگ شده</h1>
+    
+    <h4>بوت استرپ فارسی با تمام متعلقاتش نصب شدخ</h4>
+    <h4>توستی فای نصب شده</h4>
+    <h4>فونت اسوم نصب شده</h4>
+    <h4>روتر هم نصب شد</h4>
+    
+    </div>  );
+}
+ 
+export default App;
